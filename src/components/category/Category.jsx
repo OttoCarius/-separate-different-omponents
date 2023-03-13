8ca0a9c53d357cb24205bc769aa3b229daff10ab
OttoCarius <ottocarius1993@gmail.com>
2023-03-13 10:41:47 +0200

    fx

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import {BtnWrap, LogBtn, InputBtn, BurgerBtn, ButtonsBtn} from './Category.styled'
+import {
+  BtnWrap,
+  LogBtn,
+  InputBtn,
+  BurgerBtn,
+  ButtonsBtn,
+} from "./Category.styled";
 
 function Category() {
   return (
     <BtnWrap>
-         <NavLink to={'/'}>
+      <NavLink to={"/"}>
         <LogBtn>Home</LogBtn>
       </NavLink>
-      <NavLink to={'/login'}>
+      <NavLink to={"/login"}>
         <LogBtn>Login</LogBtn>
       </NavLink>
-      <NavLink to={'/searchinput'}>
+      <NavLink to={"/searchinput"}>
         <InputBtn>Inputs</InputBtn>
       </NavLink>
-      <NavLink to={'/burger'}>
+      <NavLink to={"/burger"}>
         <BurgerBtn>Burgers</BurgerBtn>
       </NavLink>
-      <NavLink to={'/buttons'}>
+      <NavLink to={"/buttons"}>
         <ButtonsBtn>Buttons</ButtonsBtn>
       </NavLink>
     </BtnWrap>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Layout from '../components/layout/Layout'
-import HomePage from '../pages/home/HomePage'
-import LoginPage from '../pages/loginPage/LoginPage'
-import SearchInput from '../pages/searchInput/SearchInput'
-import BurgerMenu from '../pages/burgerMenu/BurgerMenu'
-import Buttons from '../pages/buttons/Buttons'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Layout from "../components/layout/Layout";
+import HomePage from "../pages/home/HomePage";
+import LoginPage from "../pages/loginPage/LoginPage";
+import SearchInput from "../pages/searchInput/SearchInput";
+import BurgerMenu from "../pages/burgerMenu/BurgerMenu";
+import Buttons from "../pages/buttons/Buttons";
 
 function RouterPage() {
   return (
     <div>
-        <Layout/>
-        <Routes>
-            <Route path='/' element={<HomePage/>}/>
-            <Route path='login' element={<LoginPage/>}/>
-            <Route path='searchinput' element={<SearchInput/>}/>
-            <Route path='burger' element={<BurgerMenu/>}/>
-            <Route path='buttons' element={<Buttons/>}/>
-        </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="searchinput" element={<SearchInput />} />
+          <Route path="burger" element={<BurgerMenu />} />
+          <Route path="buttons" element={<Buttons />} />
+        </Route>
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default RouterPage
+export default RouterPage;
